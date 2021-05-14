1b44421c5315d0f1481965b082d6b3884d6715de
yatirastogi <63449149+yatirastogi@users.noreply.github.com>
2021-05-15 04:33:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Search from './search';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './weather.css';
+
 import Mainpage from './mainpage';
 
 ReactDOM.render(<Mainpage/>, document.getElementById('root'));
